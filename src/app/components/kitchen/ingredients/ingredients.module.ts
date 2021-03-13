c421ca374c13768ff09358d8d99d1a7dc53c99fe
Mohamed El-Saied <MohamedSaied.FCIH@Gmail.Com>
2021-03-13 04:18:59 +0200

    Yummly Food

--- src/app/components/kitchen/ingredients/ingredients.module.ts
+++ src/app/components/kitchen/ingredients/ingredients.module.ts
@@ -8,14 +8,12 @@ import { HeaderComponent } from '../../layout/header/header.component';
 import { SharedModule } from '../../layout/shared.module';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { DetailComponent } from './detail/detail.component';
-import { TestComponent } from './test/test.component';
 
 
 const routes: Routes = [
   { path: 'ingredients/search', pathMatch: 'full', component: SearchComponent },
   { path: 'ingredients/returned-list/:searchName', component: ReturnedListComponent },
   { path: 'ingredients/detail/:id', component: DetailComponent },
-  { path: 'ingredients/test', component: TestComponent },
 
 ];
 
@@ -24,7 +22,6 @@ const routes: Routes = [
     SearchComponent,
     ReturnedListComponent,
     DetailComponent,
-    TestComponent,
 
   ],
   imports: [CommonModule, RouterModule.forChild(routes), FormsModule, SharedModule, NgxSpinnerModule,
